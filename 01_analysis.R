a13ee915205b4c4811575a2fe25abb049d0b5eb3
Max Blockley <max.blocker55@gmail.com>
2025-08-04 11:13:41 +1200

    Unarranged flight delay means

---
+++ 01_analysis.R
@@ -0,0 +1,12 @@
+
+source("00_clean.R")
+
+library(tidyverse)
+
+flights_means <- flights_clean %>%
+  group_by(name) %>%
+  summarise(
+    avg_dep_delay = mean(dep_delay, na.rm = TRUE),
+    avg_arr_delay = mean(arr_delay, na.rm = TRUE)
+  ) 
+
